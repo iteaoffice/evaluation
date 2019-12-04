24339591d0832eb98bdcaf7f3171471c81bb13d9
Bart van Eijck <voodoo107@hotmail.com>
2019-12-04 15:23:09 +0100

    Factory rename

--- config/module.config.php
+++ config/module.config.php
@@ -12,7 +12,7 @@ namespace Evaluation;
 
 use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Evaluation\View\Factory\ViewHelperFactory;
-use General\View\Factory\LinkFactory;
+use General\View\Factory\LinkHelperFactory;
 use Zend\ServiceManager\AbstractFactory\ConfigAbstractFactory;
 use Zend\Stdlib;
 
@@ -90,12 +90,12 @@ $config = [
 
         ],
         'factories'  => [
-            View\Helper\FeedbackLink::class                  => LinkFactory::class,
-            View\Helper\EvaluationLink::class                => LinkFactory::class,
-            View\Helper\ReportLink::class                    => LinkFactory::class,
+            View\Helper\FeedbackLink::class                  => LinkHelperFactory::class,
+            View\Helper\EvaluationLink::class                => LinkHelperFactory::class,
+            View\Helper\ReportLink::class                    => LinkHelperFactory::class,
             View\Helper\Report\DownloadLink::class           => ViewHelperFactory::class,
             View\Helper\Report\PresentationLink::class       => ViewHelperFactory::class,
-            View\Helper\Report\FinalLink::class              => LinkFactory::class,
+            View\Helper\Report\FinalLink::class              => LinkHelperFactory::class,
             View\Helper\Report\Progress::class               => ConfigAbstractFactory::class,
             View\Helper\Report\Score::class                  => ConfigAbstractFactory::class,
             View\Helper\Report\VersionLink::class            => ViewHelperFactory::class,
@@ -105,7 +105,7 @@ $config = [
             View\Helper\Report\Criterion\TypeLink::class     => ViewHelperFactory::class,
             View\Helper\Report\Criterion\TopicLink::class    => ViewHelperFactory::class,
             View\Helper\Report\Criterion\VersionLink::class  => ViewHelperFactory::class,
-            View\Helper\ReviewerLink::class                  => LinkFactory::class,
+            View\Helper\ReviewerLink::class                  => LinkHelperFactory::class,
             View\Helper\Reviewer\ContactLink::class          => ViewHelperFactory::class,
         ],
     ],
